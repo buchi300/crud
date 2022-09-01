bef91cb5009cf3924004a18ea3e14eb1a076df05
Buchi Salako <buchi300@yahoo.com>
2022-09-01 14:43:10 +0100

    01/09/2022_2

--- adminViewPicture.php
+++ adminViewPicture.php
@@ -1,93 +1,102 @@
 <?php
-$pageType ="pic";
+$pageType ="media";
 require_once 'adminheader.php';
-require_once "conn.php";
-require_once "function.php";
-$sql  = "SELECT * from picture order by id desc";
-$result = $conn->query($sql);
-$i=0;
+require_once 'conn.php';
+
+$sql = "SELECT *  FROM picture order by id desc";
+	$result = $conn->query($sql);
+	$title ='';
+	$filter='';
+	 $header ='';
 ?>
 
-<main id="main">
+<!-- End Header -->
+
+  <main id="main">
 
     <!-- ======= Breadcrumbs ======= -->
     <section id="breadcrumbs" class="breadcrumbs">
-        <div class="container">
+      <div class="container">
 
-            <div class="d-flex justify-content-between align-items-center">
-                <h2>Picture</h2>                
-            </div>
-
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Portolio</h2>
+          <ol>
+            <li><a href="index.html">Home</a></li>
+            <li>Portolio</li>
+          </ol>
         </div>
+
+      </div>
     </section><!-- End Breadcrumbs -->
 
-    <!-- ======= About Us Section ======= -->
-    <section id="about-us" class="about-us">
-        <div class="container">
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+      <div class="container">
 
-            <div class="row no-gutters m-3">
+        <div class="row" data-aos="fade-up">
+          <div class="col-lg-12 d-flex justify-content-center">
+            <ul id="portfolio-flters">
+              <li data-filter="*" class="filter-active">All</li>
+              <li data-filter=".filter-med">Medical</li>
+              <li data-filter=".filter-com">Community</li>
+			  <li data-filter=".filter-pro">Projects</li>
+            </ul>
+          </div>
+        </div>
 
+        <div class="row portfolio-container" data-aos="fade-up">
 
+        <?php
+			  if ($result->num_rows > 0) {
+			     while($row = $result->fetch_assoc()) {
+					 
+					 if ($row['title']=='medical'){
+						 $title ='Medical';
+						 $filter='filter-med';
+						 $header ='Medical Outreach';
+					 } 
+					 elseif ($row['title']=='community')
+					 {
+						 $title ='Community';
+						 $filter='filter-com';
+						 $header ='Community Outreach';
+					 }
+					 else{
+						 $title ='Projects';
+						 $filter='filter-pro';
+						 $header ='Projects';
+					 }
+					 
+				?>
 
-                <div class="col-md-12 mb-3  mx-auto">
-                    <div class="row">
-
-                    <?php
-                      $i=1;
-                      if ($result->num_rows > 0) {
-                         while($row = $result->fetch_assoc()) {?>
-
-                        <div class="col-md-4 mb-3 ">
-                            <div class="card" style="width: 24rem;">
-                                <div class="card-body">
-                                    <h5 class="card-title"> <strong><a href="#"> <?php echo $row["title"]?></strong></a>
-                                    </h5>
-                                    <div class="border-bottom border-3 border-danger "></div>
-                                    <h6 class="card-subtitle m-2 ">
-                                      <!--  Date: <strong><?php echo $row["date"]?></strong>-->
-                                    </h6>
-                                    <div class="border-bottom border-3 "></div>
-                                    <p class="card-text"> <img src="assets/picture/<?php echo $row["id"].$row["ext"]?>" class="img-fluid"></p>
-                                    <p><?php echo $row["discription"]?></p>
-                                    <a href="adminEditPicture.php?id=<?php echo $row["id"]?>" class="btn btn-primary">EDIT</a>
-                                    <a href="adminDeletePicture.php?id=<?php echo $row["id"]?>" class="btn  btn-primary">Delete</a>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <?php 
-                    if($i%3==0){
-                      ?>
-                      </div>
-                      <div class="row">
-                      <?php
-                    }
-                    $i++;
-
-                    }
-                    } else {
-                        echo "No Picture posted";
-                    }
-                    ?>
-
-
-
-                    </div>
-
-
-                </div>
-
-
-
-
+          <div class="col-lg-4 col-md-6 portfolio-item <?php echo $filter?>">
+            <img src="assets/picture/<?php echo $row["id"].$row["ext"] ?>" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <h4><?php echo $header ?></h4>
+              <a href="assets/picture/<?php echo $row["id"].$row["ext"] ?>" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="<?php echo $title?>"><i class="bx bx-plus"></i></a>
+              <p><?php echo $row['discription'] ?></p> 
+              <a href="adminEditPicture.php?id=<?php echo $row["id"]?>" class="link-info ">Edit </a>|
+              <a href="adminDeletePicture.php?id=<?php echo $row["id"]?>" class="link-danger">Delete</a>
             </div>
+            
+          </div>
+
+          <?php
+	
+				 }
+			} else {
+				echo "No picture posted";
+			}
+			$conn->close();
+					?>          
 
         </div>
-    </section><!-- End About Us Section -->
 
+      </div>
+    </section><!-- End Portfolio Section -->
 
-</main><!-- End #main -->
+  </main><!-- End #main -->
 
-<?php 
-require_once 'adminfooter.php';
+ <?php
+ require_once 'adminfooter.php'
  ?>
